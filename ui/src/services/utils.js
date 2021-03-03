ea7f66413664f1aabcc9bcf2a81bf06eb884179d
Yingjie Guo <yguo@appen.com>
2021-03-03 13:41:39 +0800

    use Intl

--- ui/src/services/utils.js
+++ ui/src/services/utils.js
@@ -1,3 +1,4 @@
 export function numberWithCommas(x) {
-  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+  // return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+  return Intl.NumberFormat().format(x);
 }
