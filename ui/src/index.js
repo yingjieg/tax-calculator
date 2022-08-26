33f9a6d55c2285b6bcfe31ab39e090ccb04f7b4f
Yingjie Guo <yguo@appen.com>
2022-08-26 13:50:57 +0800

    pkg upgrade & update social base

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "semantic-ui-css/semantic.min.css";
-import "./index.css";
+import { createRoot } from "react-dom/client";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
